66618d06e0f8154af8ac3f191cde5756525a89d1
Zanca <Zanca@example.com>
2015-06-16 12:26:00 +0200

    gradle

--- Nearby/src/com/progetto/nearby/detailPlaces/DetailPlaceActivity.java
+++ Nearby/src/com/progetto/nearby/detailPlaces/DetailPlaceActivity.java
@@ -179,18 +179,9 @@ public class DetailPlaceActivity extends Activity {
 					@Override
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
-						// Create a Uri from an intent string. Use the result to create an Intent.
-						//google.streetview:cbll=
 						String url = "http://maps.google.com/maps?"
 								+ "daddr=" + place.lat + "," + place.longit;
-						//Uri gmmIntentUri = Uri.parse("geo:" + place.lat + "," + place.longit);
-
-						// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
 						Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-						// Make the Intent explicit by setting the Google Maps package
-						//mapIntent.setPackage("com.google.android.apps.maps");
-
-						// Attempt to start an activity that can handle the Intent
 						startActivity(mapIntent);
 					}
 				});
