97a1310610d9e67b0fb04ebe75b605b979ca9a61
Andreag16 <TSAIM040@tecnicosuperiorekennedy.it>
2015-07-02 13:12:57 +0200

    aaa

--- Nearby/src/com/progetto/nearby/home/HomeActivity.java
+++ Nearby/src/com/progetto/nearby/home/HomeActivity.java
@@ -10,7 +10,6 @@ import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -126,7 +125,7 @@ public class HomeActivity extends AppCompatActivity implements
 				break;
 			case 4:
 			{
-				String url = "http://nearby.altervista.org";
+				String url = "http://nearby.altervista.org/session/administration.php";
 				Intent areaRiservataIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 				startActivity(areaRiservataIntent);
 			}
